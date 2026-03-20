6630edb4ff47912156700d27d1ece3e5fb73e4d2
celavin <2745487674@qq.com>
2026-03-20 22:05:51 +0800

    增几个todo

--- src/main/java/com/celavin/badmintonepic/service/PlayerService.java
+++ src/main/java/com/celavin/badmintonepic/service/PlayerService.java
@@ -4,8 +4,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.celavin.badmintonepic.model.entity.Player;
 
 import java.util.List;
-
+//todo 待解耦
 public interface PlayerService extends IService<Player> {
+    //重置数据库?
     List<Player> initWorld(int num);
+    //临时生成一批球员
     List<Player> generatePlayerTemp(int num);
 }
--- src/main/java/com/celavin/badmintonepic/util/PlayerGenerator.java
+++ src/main/java/com/celavin/badmintonepic/util/PlayerGenerator.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Random;
-
+//todo 待解耦
 public class PlayerGenerator {
     private static final Random random = new Random();
 
