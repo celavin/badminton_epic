ce0b8179c6f4a4b0d72ab29377a2b8ca53013a91
celavin <2745487674@qq.com>
2026-03-20 22:28:30 +0800

    搞懂了测试类原理,下一步首要目标:调整轮次顺序,渲染对阵图

--- src/test/java/com/celavin/badmintonepic/TournamentTest.java
+++ src/test/java/com/celavin/badmintonepic/TournamentTest.java
@@ -4,13 +4,10 @@ import com.celavin.badmintonepic.engine.simulator.MatchEngine;
 import com.celavin.badmintonepic.engine.tournament.Tournament;
 import com.celavin.badmintonepic.engine.tournament.format.KnockOutFormat;
 import com.celavin.badmintonepic.enums.TournamentLevel;
-import com.celavin.badmintonepic.model.dto.GameScore;
 import com.celavin.badmintonepic.model.dto.MatchNode;
-import com.celavin.badmintonepic.model.dto.RawMatchResult;
 import com.celavin.badmintonepic.model.entity.Player;
 import com.celavin.badmintonepic.service.MatchSettlementService;
 import com.celavin.badmintonepic.service.PlayerService;
-import com.celavin.badmintonepic.util.TournamentPrinter;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,7 +24,8 @@ public class TournamentTest {
     @Autowired
     MatchSettlementService matchSettlementService;
     @Test
-    void test(){
+    //单场细节
+    void singleDetailedTest(){
         HashMap<String,Integer> championCounts = new HashMap<>();
 
         //List<Player> list = playerService.generatePlayerTemp(8);
@@ -42,6 +40,19 @@ public class TournamentTest {
         }
         championCounts.put(t.getChampion().getName(),championCounts.getOrDefault(t.getChampion().getName(),0)+1);
 
+
+        System.out.println(championCounts);
+    }
+    @Test
+    //多场看实力
+    void manyTimestest(){
+        HashMap<String,Integer> championCounts = new HashMap<>();
+        List<Player> list = playerService.list();
+        for (int i = 0; i < 10000; i++) {
+            Tournament t = new Tournament("Beijing", TournamentLevel.RANKED,new KnockOutFormat(),list);
+            t.simulateAll(matchEngine,matchSettlementService);
+            championCounts.put(t.getChampion().getName(),championCounts.getOrDefault(t.getChampion().getName(),0)+1);
+        }
 
         System.out.println(championCounts);
 
